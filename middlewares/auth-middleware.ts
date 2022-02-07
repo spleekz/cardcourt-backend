76de0fca84a00ffc0c88fb399a2bb87936e1c991
spleekz <Shutnik-12K@yandex,ru>
2022-02-07 20:06:03 +0300

    add card and auth middlewares

---
+++ middlewares/auth-middleware.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import config from '../config.json'
+import { Response } from '../api/server-utility-types'
+import { UserModel } from '../models/user'
+import { Id } from '../api/api-types'
+
+export const authMiddleware = async (
+  req: express.Request,
+  res: Response,
+  next: express.NextFunction
+) => {
+  if (!req.headers.authorization) {
+    return res.json({ message: 'Ошибка авторизации!' })
+  }
+
+  const token = req.headers.authorization.split(' ')[1]
+
+  if (!token) {
+    return res.json({ message: 'Ошибка авторизации!' })
+  }
+
+  const userId = (jwt.verify(token, config.secret) as Id)._id
+
+  const user = await UserModel.findById(userId)
+
+  if (!user) {
+    return res.status(404).json({ message: `Пользователь с id ${userId} не существует!` })
+  }
+
+  req.user = user
+  next()
+}
---
+++ middlewares/card-middleware.ts
@@ -0,0 +1,27 @@
+import express, { NextFunction } from 'express'
+import mongoose from 'mongoose'
+import { Request, Response } from '../api/server-utility-types'
+import { CardModel } from '../models/card'
+
+export async function cardMiddleware<RequestBody extends { _id: string }>(
+  req: Request<{}, RequestBody>,
+  res: Response,
+  next: NextFunction
+) {
+  const { _id } = req.body
+
+  const isIdValid = mongoose.Types.ObjectId.isValid(_id)
+
+  if (!isIdValid) {
+    return res.status(400).json({ message: 'Неверный формат id карточки' })
+  }
+
+  const thisCard = await CardModel.findById(_id)
+
+  if (!thisCard) {
+    return res.status(404).json({ message: `Не существует карточки с id ${_id}!` })
+  }
+
+  req.card = thisCard
+  next()
+}
